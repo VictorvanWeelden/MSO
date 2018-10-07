ddc4db2b497a90fa1c6af48760c12298ab6c5b26
Victor <victor.van.weelden@gmail.com>
2018-10-07 21:03:37 +0200

    wekjvf

---
+++ Lab3/Lab3/NormalTicket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public class NormalTicket : Ticket
+    {
+        public bool Day;
+        public bool Single = false;
+        public bool International = false;
+        public NormalTicket(string from, string to, int classes, float price, bool today, bool single, bool international) : base(from, to, classes, price)
+        {
+            Day = today;
+            Single = single;
+            International = international;
+        }
+    }
+}
